3028c734a9364b589a38d6a1d1588daf164cb375
Krastin Krastev <krastin@hashicorp.com>
2023-12-04 19:01:48 +0200

    revamp with helm from proxy-metrics guide repo

--- variables.tf
+++ variables.tf
@@ -3,3 +3,15 @@ variable "region" {
   type        = string
   default     = "us-east-2"
 }
+
+variable "consul_chart_version" {
+  type        = string
+  description = "The Consul Helm chart version to use"
+  default     = "1.3.0"
+}
+
+variable "consul_version" {
+  type        = string
+  description = "The Consul version to use"
+  default     = "1.17.0"
+}
